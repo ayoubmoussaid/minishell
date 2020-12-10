665aadbf33e3716539e395de97740dc0bd39180d
Fatima ezzahra Mehdaoui <fmehdaou@e2r7p9.1337.ma>
2020-12-10 15:00:48 +0100

    hd

--- src/headers/minishell.h
+++ src/headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: fmehdaou <fmehdaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 14:25:05 by amoussai          #+#    #+#             */
-/*   Updated: 2020/12/08 12:26:38 by fmehdaou         ###   ########.fr       */
+/*   Updated: 2020/12/10 14:59:05 by fmehdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,24 @@
 #include "../../gnl/get_next_line.h"
 
 
-typedef struct	s_shell{
-	char	**env;
-	FILE	*debug_file;
-}				t_shell;
+# define COMMA 0
+# define QUOTES 1
+# define QUOTED 1
+
+
+
+static char		*(g_mishell_err[]) =
+{
+	"Syntax error near unexpected token `;;' ",
+	"Syntax error near unexpected token  `\"' ",
+	"Syntax error near unexpected token  `\'' ",
+	""
+};
+
+
+
+
+
 
 
 typedef struct	s_getl{
@@ -32,8 +46,35 @@ typedef struct	s_getl{
 	int fd;
 	int r;
 	char **sp;
-	char *trim;
+	char *line_t;
+	char **sps;
+	int quote;
+	char *zeros;
+	int err;
 }				t_getl;
+
+
+typedef struct	s_cmd{
+	char			*c;
+	char			**args;
+	int				pipe;
+	char			*infile_s;
+	char			*infile_d;
+	int				outfile;
+}				t_cmd;
+
+typedef struct	s_lst
+{
+	t_cmd			*content;
+	struct s_list	*next;
+}				t_lst;
+
+
+typedef struct	s_shell{
+	char	**env;
+	FILE	*debug_file;
+}				t_shell;
+
 
 
 void	ft_env(char **env);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fmehdaou <fmehdaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 14:33:21 by amoussai          #+#    #+#             */
-/*   Updated: 2020/12/08 14:55:44 by fmehdaou         ###   ########.fr       */
+/*   Updated: 2020/12/10 14:59:35 by fmehdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,39 +40,180 @@ void	my_env(char **env, t_shell *shell)
 
 
 
-void	printsp(t_getl *getl)
-{
-	int i = -1;
+// void	printsp(char **sp)
+// {
+// 	int i = -1;
 
-	while(getl->sp[++i])
-	{
-		getl->trim = ft_strtrim(getl->sp[i] , " ");
-		printf("sp[%d]|%s|\n",i, getl->trim);
-	}
-		
+// 	printf("--------start--------\n");
+// 	while(sp[++i])
+// 		printf("|%s|\n",sp[i]);
+// 	printf("---------end---------\n");
+// }
+
+
+// int	check_comma(t_getl *getl)
+// {
+// 	int i;
+// 	int sq;
+// 	int dq;
+
+// 	i = -1;
+// 	dq = 34;
+// 	sq = 39;
+
+// 	while(getl->line[++i])
+// 	{
+// 		if (getl->line[i] == ';')
+// 		{
+// 			if ((getl->line[i - 1] == (char)dq && getl->line[i + 1] == (char)dq) || 
+// 			(getl->line[i - 1] == (char)sq && getl->line[i + 1] == (char)sq))
+// 				return (1);
+// 		}
+// 	}
+// 	return (0);
+// }
+
+
+// int indexof_char(char *str, char *c)
+// {
+// 	int i;
+// 	char *s;
+
+// 	i = -1;
+// 	s = ft_strtrim(str, c);
+// 	while(s[++i])
+// 	{
+// 		if (s[i] == c[0])
+// 			return (i);
+// 	}
+// 	return (-1);
+// }
+
+// int skip_char(char *s, char *c, int start)
+// {
+// 	int i;
+
+// 	i = start;
+// 	while(s[i])
+// 	{
+// 		if (s[i] == c[0] && s[i + 1] != c[0])
+// 			return (i+1);
+// 		i++;
+// 	}
+// 	return (-1);
+// }
+
+
+
+
+
+
+
+
+// void	foreach_cmd(char *sp)
+// {
+	
+// 	t_cmd *cmd;
+// 	char *args;
+// 	int in;
+	
+
+// 	// int   in;
+
+// 	// in = indexof_char(sp, " ");
+// 	cmd = (t_cmd*)malloc(sizeof(t_cmd));
+// 	cmd->c = ft_substr(sp, 0, (in = indexof_char(sp, " ")));
+// 	in = skip_char(sp, " ", in);
+// 	args = ft_substr(sp, in, ft_strlen(sp) - in);
+// 	printf("|cmd->c:|%s|\n|args:|%s|\n\n",cmd->c, args);
+	
+
+
+	
+	
+
+	
+// }
+
+
+
+
+// void	get_cmd(t_getl *getl)
+// {
+// 	int i;
+
+// 	i = -1;
+// 	getl->sp = ft_split(getl->line_t, ';');
+// 	while(getl->sp[++i])
+// 	{
+// 		foreach_cmd(getl->sp[i]);
+// 		free(getl->sps);
+// 	}
+	
+	
+// }
+
+
+void	errrors(char *err)
+{
+	ft_putendl_fd(err, 1);
 }
 
 
-int	check_comma(t_getl *getl)
+
+int	check_comma(t_getl *getl, int *i)
 {
-	int i;
-	int sq;
-	int dq;
-
-	i = -1;
-	dq = 34;
-	sq = 39;
-
-	while(getl->line[++i])
+	while(getl->line_t[++(*i)])
 	{
-		if (getl->line[i] == ';')
+		if (getl->line_t[*i] == ';')
 		{
-			if ((getl->line[i - 1] == (char)dq && getl->line[i + 1] == (char)dq) || 
-			(getl->line[i - 1] == (char)sq && getl->line[i + 1] == (char)sq))
-				return (1);
+			getl->zeros[*i] = '1'; //;
+			getl->line_t[*i] = '1'; //;
+		}
+		else if (getl->line_t[*i] == '"')
+		{
+			getl->quote = 0;
+			return (*i);
 		}
 	}
-	return (0);
+	return (-1);
+}
+
+
+
+
+void	tozeros(char *s, int len)
+{
+	while(len--)
+		*s++ =  '0';
+} 
+
+int	toclear(t_getl *getl)
+{
+	int i;
+
+	getl->quote = 0;
+	i = -1;
+	while(getl->line_t[++i])
+	{
+		if (getl->line_t[i] == '"')
+		{
+			getl->quote = 1;
+			i = check_comma(getl, &i);
+		}
+		if (getl->quote == 1)
+		{
+			//printf("error\n");
+			return (-1);// failed
+		}
+
+		// else if (getl->line_t[i] == (char)39)
+		// {
+		// 	printf("syntax error\n");
+		// }
+				
+	}
+	return (1);
 }
 
 
@@ -84,22 +225,26 @@ int     main(int argc, char **argv, char **env)
 {
 	t_shell *shell;
 	t_getl *getl;
+	int len;
+
+	//t_lst list;
 
 	shell = (t_shell*)malloc(sizeof(t_shell));
 	getl = (t_getl*)malloc(sizeof(t_getl));
+	getl->err = 1;
 
 	shell->debug_file = fopen("debug.txt", "w");
 	if (argc > 1)
 		argv = NULL;
 	my_env(env, shell);
 	//ft_env(shell.env);
-	ft_pwd();
+	// ft_pwd();
 	//ft_cd(&shell, "..");
 	//ft_cd(&shell, "minishell");
 	//ft_cd(&shell, "/freak");
 	//fprintf(shell->debug_file, "hi fatima %d\n", 22);
-	char *tab[] = {"$OLDPWD", (void*)0};
-	ft_echo(shell, tab);
+	// char *tab[] = {"$OLDPWD", (void*)0};
+	// ft_echo(shell, tab);
 
 
 
@@ -109,14 +254,28 @@ int     main(int argc, char **argv, char **env)
 		if (get_next_line(0, &getl->line) > 0)
 		{
 			
-			printf("line|%s|\n", getl->line);
-			if (!(check_comma(getl)))
+			// getl->line_t =  ft_strtrim(getl->line, " ");
+			// len = ft_strlen(getl->line_t);
+
+			// get_cmd(getl);
+			getl->line_t =  ft_strtrim(getl->line, " ");
+			printf("|%s|\n",getl->line_t);
+			len = ft_strlen(getl->line_t);
+			getl->zeros = (char*)malloc(sizeof(char) * (len + 1));
+			tozeros(getl->zeros, len);
+			printf("|%s|\n\n\n\n",getl->zeros);
+			if ((getl->err = toclear(getl)) == -1)
 			{
-				getl->sp = ft_split(getl->line, ';');
-				printsp(getl);
+				errrors(g_mishell_err[QUOTES]);
+				continue;// show err and continue to the next cmd
 			}
-			else 
-				printf("should not be splited with ;");
+				
+			printf("|%s|\n",getl->zeros);
+			printf("|%s|\n",getl->line_t);
+
+			
+				
+			
 			//printsp(getl);
 
 		}
